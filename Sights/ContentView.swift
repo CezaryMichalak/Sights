70894c42a09fe85dac929edc2432b12916bcc10a
CezaryMichalak <68953776+CezaryMichalak@users.noreply.github.com>
2020-11-05 15:09:15 +0100

    Initial Commit

---
+++ Sights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sights
+//
+//  Created by Cezary Michalak on 05/11/2020.
+//  Copyright © 2020 Cezary Michalak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
